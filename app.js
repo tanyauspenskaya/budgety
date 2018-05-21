1ca5e9592816c5e1a0b12f2fa298dc76cac26105
Tatiana Uspenskaia <tanyauspenskaya@gmail.com>
2018-05-21 19:25:30 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// BUDGET CONTROLLER
+var budgetController = (function(){
+
+//
+
+
+})();
+
+
+
+
+
+// UI CONTROLLER
+var UIController = (function(){
+
+})();
+
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+
+  document.querySelector('.add__btn').addEventListener('click', function() {
+    
+    // 1. get the field input data
+
+    // 2. add the new item to the budget controller
+
+    // 3. add the new item to the UI
+
+    // 4. Calculate the budget 
+
+    // 5. Display the budget on the UI
+
+  });
+
+})(budgetController, UIController);
+
